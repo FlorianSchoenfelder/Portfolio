1f6bb4f822b21721136523b27128364f47f18f31
FlorianSchoenfelder <f.schoenfelder22@gmail.com>
2024-07-10 09:46:13 +0200

    about me/ skill section almost done

--- src/app/main-content/about-me-section/about-me-section.component.ts
+++ src/app/main-content/about-me-section/about-me-section.component.ts
@@ -15,7 +15,6 @@ gsap.registerPlugin(ScrollTrigger);
 })
 export class AboutMeSectionComponent {
 
-  @ViewChild('ripple') rippleBtn!: ElementRef;
 
   ngOnInit(): void {
     gsap.from('#work-together', {
@@ -23,7 +22,7 @@ export class AboutMeSectionComponent {
         trigger: '#work-together',
         // markers: true,
         start: 'top bottom',
-        toggleActions: 'restart none none none'
+        toggleActions: 'restart none restart none'
       }, // start animation when ".box" enters the viewport
       x: -100,
       opacity: 0,
@@ -32,27 +31,6 @@ export class AboutMeSectionComponent {
 
 
 
-    //ripple btn versuch
-    // const rippleBtn = document.getElementById("ripple");
-    let ripples = document.createElement("span");
-    let cleartimeout:any;
-
-    //if want this effect when clicked then just add click insted of mouseover event
-    this.rippleBtn.nativeElement.addEventListener("mouseover", function (e:any) {
-      let x = e.clientX - e.target.offsetLeft;
-      let y = e.clientY - e.target.offsetTop;
-      ripples.style.left = x + "px";
-      ripples.style.top = y + "px";
-      e.target.appendChild(ripples);
-
-      cleartimeout = setTimeout(() => {
-        ripples.remove();
-      }, 1000);
-    });
-
-    this.rippleBtn.nativeElement.addEventListener("mouseout", function () {
-      ripples.remove();
-    });
 
 
 
--- src/app/main-content/skill-section/skill-section.component.ts
+++ src/app/main-content/skill-section/skill-section.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { gsap } from "gsap";
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-skill-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill-section.component.html',
   styleUrl: './skill-section.component.scss'
 })
 export class SkillSectionComponent {
 
+  public skillSet: string[] = [
+    'Angular',
+    'Rest-Api',
+    'CSS',
+    'HTML',
+    'Firebase',
+    'GIT',
+    'JavaScript',
+    'TypeScript',
+    'Material Design',
+    'Scrum',
+  ];
+
+  ngOnInit(): void {
+    gsap.from('#headline-skill', {
+      scrollTrigger: {
+        trigger: '#headline-skill',
+        // markers: true,
+        start: 'top bottom',
+        toggleActions: 'restart none restart none'
+      }, // start animation when ".box" enters the viewport
+      x: -100,
+      opacity: 0,
+      duration: 1
+    });
+  }
 }
